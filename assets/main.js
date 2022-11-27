33dbd2efd62c5f3b5ddc552286485eee240cf0fa
NicholasHM05 <nhmamberger@gmail.com>
2022-11-27 16:48:49 -0600

    added some pseudo code to js and added links and pseudo code to the index

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+//We need to store the value of the input\\
+// The function could look like this. let town =$("#searchWord").val();
+// Next we will need to store the api key
+const apiKey = "9dd247337ad4b6b047a2f9a30dacaf93"

8424c4b3bfeed87b7cd8cc6087ef98ecd06e8a51
NicholasHM05 <nhmamberger@gmail.com>
2022-11-27 19:30:12 -0600

    the forecast will show when the button is hit

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,20 @@
 //We need to store the value of the input\\
-// The function could look like this. let town =$("#searchWord").val();
+// The function could look like this. 
+let town =$("#searchWord").val();
 // Next we will need to store the api key
 const apiKey = "9dd247337ad4b6b047a2f9a30dacaf93"
+
+let date = new Date();
+
+$("SearchTerm").keypress(function(event) {
+
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        $("#searchBtn").click('show');
+    }
+});
+
+$("#searchBtn").on("click",function(){
+
+    $('#forecastH5').addClass('show');
+})

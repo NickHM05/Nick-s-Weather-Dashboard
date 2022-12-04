547ca9b3c73a0dff617f7f44ed65e2144d572a63
NicholasHM05 <nhmamberger@gmail.com>
2022-12-04 15:53:11 -0600

    still not working

--- assets/main.js
+++ assets/main.js
@@ -9,8 +9,8 @@
 function addResult() {
 
     inputCity = document.getElementById("myInput").value;
-    historyList = getStorage();
-    var CitySearch=$("<div>")
+    historyList = getInfo();
+    var CitySearch = $("<div>")
     CitySearch.attr('id', inputCity)
     CitySearch.text(inputCity)
     CitySearch.addClass("h4")
@@ -18,16 +18,16 @@ function addResult() {
     if (historyList.includes(inputCity) === false) {
         $(".history").append(CitySearch)
     }
-    $('.subtext').attr("style", "display:inline")
+    $('.subtitle').attr("style", "display:inline")
     addInfo(inputCity);
 
 };
 //adding an event listener to search history code item
-$(".history").on('click', function(event){
+$(".history").on('click', function (event) {
     event.preventDefault();
-    $(".subtitle").attr("style","display:inline")
-     document.getElementById("myInput").value =  event.target.id;
-    getResult(); 
+    $(".subtitle").attr("style", "display:inline")
+    document.getElementById("myInput").value = event.target.id;
+    getResult();
 });
 
 //adding an event listener to search button to make my code more simple to understand this time and the event listener will go to the search button
@@ -38,14 +38,15 @@ document.getElementById("searchBtn").addEventListener("click", getResult);
 //THEN I am presented with the city name, the date, the icon of weather conditions, the temperature, the humidity, the wind speed
 function getResult() {
 
-    $("forecast").empty();
-    $(".currentCity").empty();
+    $("five-day").empty();
+    $(".city").empty()
+
     inputCity = document.getElementById("myInput").value;
     var countryCode = "US";
     var cityCodex = inputCity;
 
-    var Longi;
-    var Latti;
+    var geoLon;
+    var geoLat;
 
     var cityname = $("<h>")
     cityname.addClass("h3")
@@ -54,18 +55,20 @@ function getResult() {
     var wind = $("<div>")
     var icon = $("<img>")
     icon.addClass("icon");
-    var datetobe = $("<div>")
+    var dateTime = $("<div>")
 
-    $(".forecast").addClass("list-group")
-    $(".forecast").append(cityname)
-    $(".forecast").append(datetobe)
-    $(".forecast").append(icon)
-    $(".forecast").append(temp)
-    $(".forecast").append(wind)
-    $(".forecast").append(humidity)
+    $(".city").addClass("list-group")
+    $(".city").append(cityname)
+    $(".city").append(dateTime)
+    $(".city").append(icon)
+    $(".city").append(temp)
+    $(".city").append(wind)
+    $(".city").append(humidity)
 
     var thegeourl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityCodex + "," + countryCode + "&limit=5&appid=9dd247337ad4b6b047a2f9a30dacaf93"
+    console.log(thegeourl)
 
+    //We then pass the requestUrl variable as an argument to the fetch() method, like in the following code:
     fetch(thegeourl)
 
         //converts the response into JSON. This will return the JSON formatted response as ahead in the code
@@ -74,68 +77,70 @@ function getResult() {
         })
 
         .then(function (data) {
-            Longi = data[0].lon;
-            Latti = data[0].lat;
-        })
+            geoLon = data[0].lon;
+            geoLat = data[0].lat;
 
-    //use Latti and Longi to fetch the weather
-    var weather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + Latti + "&lon=" + Longi + "&exclude=minutely,hourly,alerts&units=imperial&appid=9dd247337ad4b6b047a2f9a30dacaf93";
+            //console.log(geoLon)
+            //console.log(geoLat)
+            //use Latti and Longi to fetch the weather
+            var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + geoLat + "&lon=" + geoLon + "&exclude=minutely,hourly,alerts&units=imperial&appid=9dd247337ad4b6b047a2f9a30dacaf93";
 
-    fetch(weather)
+            fetch(weatherUrl)
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            //console.log(data)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data)
 
-            weatherIcon = data.current.weather[0].icon;
-            imgSrc = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
-            icon.attr('src', imgSrc)
+                    weatherIcon = data.current.weather[0].icon;
+                    imgSrc = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+                    icon.attr('src', imgSrc)
 
-            cityname.text(cityCodex);
-            //translate utc to the date
-            var date = new Date(date.current.dt * 1000);
-            dateTime.text("(" + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + ")");
+                    cityname.text(cityCodex);
+                    //translate utc to the date
+                    var date = new Date(date.current.dt * 1000);
+                    dateTime.text("(" + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + ")");
 
-            temp.text("Temperature: " + data.curent.temp + "°F");
-            humidity.text("Humidity: " + data.current.humidity + " %");
-            wind.text("Wind Speed: " + data.current.wind_speed + " MPH");
+                    temp.text("Temperature: " + data.curent.temp + "°F");
+                    humidity.text("Humidity: " + data.current.humidity + " %");
+                    wind.text("Wind Speed: " + data.current.wind_speed + " MPH");
 
-            // WHEN I view future weather conditions for that city
-            // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-            //using the data from previous fetch and display the 5 day weather data
-            for (var i = 1; i < 6; i++) {
+                    // WHEN I view future weather conditions for that city
+                    // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+                    //using the data from previous fetch and display the 5 day weather data
+                    for (var i = 1; i < 6; i++) {
 
-                var Container = $("<div>")
-                this["futureDate" + i] = $("<h>")
-                this["futureIcon" + i] = $("<img>")
-                this["futureTemp" + i] = $("<div>")
-                this["futureWind" + i] = $("<div>")
-                this["futureHumidity" + i] = $("<div>")
+                        var blueContainer = $("<div>")
+                        this["futureDate" + i] = $("<h>")
+                        this["futureIcon" + i] = $("<img>")
+                        this["futureTemp" + i] = $("<div>")
+                        this["futureWind" + i] = $("<div>")
+                        this["futureHumidity" + i] = $("<div>")
 
-                //translate utc to the date
-                this["forecastDay" + i] = new Date(data.daily[i].dt * 1000);
+                        //translate utc to the date
+                        this["forecastDay" + i] = new Date(data.daily[i].dt * 1000);
 
-                (this["futureDate" + i]).text(((this["forecastDay" + i]).getMonth() + 1) + "/" + (this["forecastDay" + i]).getDate() + "/" + (this["forecastDay" + i]).getFullYear());
-                (this["futureTemp" + i]).text("Temperature: " + data.daily[i].temp.day + " F");
-                (this["futureWind" + i]).text("Wind: " + data.daily[i].wind_speed + " MPH");
-                (this["futureHumidity" + i]).text("Humidity: " + data.daily[i].humidity + " %");
-                (this["weatherIcon" + i]) = data.daily[i].weather[0].icon;
+                        (this["futureDate" + i]).text(((this["forecastDay" + i]).getMonth() + 1) + "/" + (this["forecastDay" + i]).getDate() + "/" + (this["forecastDay" + i]).getFullYear());
+                        (this["futureTemp" + i]).text("Temperature: " + data.daily[i].temp.day + " F");
+                        (this["futureWind" + i]).text("Wind: " + data.daily[i].wind_speed + " MPH");
+                        (this["futureHumidity" + i]).text("Humidity: " + data.daily[i].humidity + " %");
+                        (this["weatherIcon" + i]) = data.daily[i].weather[0].icon;
 
-                DateimgSrc = "https://openweathermap.org/img/wn/" + (this["weatherIcon" + i]) + ".png";
-                (this["futureIcon" + i]).attr('src', DateimgSrc)
+                        DateimgSrc = "https://openweathermap.org/img/wn/" + (this["weatherIcon" + i]) + ".png";
+                        (this["futureIcon" + i]).attr('src', DateimgSrc)
 
-                $(".five-day").append(blueContainer)
-                blueContainer.append((this["futureDate" + i]));
-                blueContainer.append((this["futureIcon" + i]));
-                blueContainer.append((this["futureTemp" + i]));
-                blueContainer.append((this["futureWind" + i]));
-                blueContainer.append((this["futureHumidity" + i]));
+                        $(".five-day").append(blueContainer)
+                        blueContainer.append((this["futureDate" + i]));
+                        blueContainer.append((this["futureIcon" + i]));
+                        blueContainer.append((this["futureTemp" + i]));
+                        blueContainer.append((this["futureWind" + i]));
+                        blueContainer.append((this["futureHumidity" + i]));
 
-                blueContainer.addClass("weather-card")
-            }
+                        blueContainer.addClass("weather-card")
+                    }
 
+                })
         })
 }
 
@@ -145,7 +150,7 @@ function getResult() {
 // THEN I am again presented with current and future conditions for that city
 
 //get local storage info
-function getStorage() {
+function getInfo() {
     var currentList = localStorage.getItem("city");
     if (currentList !== null) {
         freshList = JSON.parse(currentList);
@@ -156,10 +161,10 @@ function getStorage() {
     return freshList;
 }
 //add info to local
-function addInfo (n) {
-    var addedList = getStorage();
+function addInfo(n) {
+    var addedList = getInfo();
 
-    if (historyList.includes(inputCity) === false){
+    if (historyList.includes(inputCity) === false) {
         addedList.push(n);
     }
 
@@ -167,7 +172,7 @@ function addInfo (n) {
 };
 //render history
 function renderInfo() {
-    var historyList = getStorage();
+    var historyList = getInfo();
     for (var i = 0; i < historyList.length; i++) {
         var inputCity = historyList[i];
         var searchCity = $("<div>")

e7331f02f68e2393ade209b234157677c56d4db7
NicholasHM05 <nhmamberger@gmail.com>
2022-12-04 00:43:19 -0600

    tried rewriting my code again and it didnt work

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,186 @@
-//We need to store the value of the input\\
+//We need to store the value of the input\\putCityputCity
 // The function could look like this. 
-let city = $("#SearchTerm").val();
-// Next we will need to store the api key
-const apiKey = "&appid=9dd247337ad4b6b047a2f9a30dacaf93";
+
+//let city = $("#SearchTerm").val();
+// the api key
+//const apiKey = "&appid=9dd247337ad4b6b047a2f9a30dacaf93";
+
+//our new function to collect the user input and display it in search history over old one cuz this one will be responsive
+function addResult() {
+
+    inputCity = document.getElementById("myInput").value;
+    historyList = getStorage();
+    var CitySearch=$("<div>")
+    CitySearch.attr('id', inputCity)
+    CitySearch.text(inputCity)
+    CitySearch.addClass("h4")
+
+    if (historyList.includes(inputCity) === false) {
+        $(".history").append(CitySearch)
+    }
+    $('.subtext').attr("style", "display:inline")
+    addInfo(inputCity);
+
+};
+//adding an event listener to search history code item
+$(".history").on('click', function(event){
+    event.preventDefault();
+    $(".subtitle").attr("style","display:inline")
+     document.getElementById("myInput").value =  event.target.id;
+    getResult(); 
+});
+
+//adding an event listener to search button to make my code more simple to understand this time and the event listener will go to the search button
+document.getElementById("searchBtn").addEventListener("click", addResult);
+document.getElementById("searchBtn").addEventListener("click", getResult);
+
+//WHEN I view current weather...
+//THEN I am presented with the city name, the date, the icon of weather conditions, the temperature, the humidity, the wind speed
+function getResult() {
+
+    $("forecast").empty();
+    $(".currentCity").empty();
+    inputCity = document.getElementById("myInput").value;
+    var countryCode = "US";
+    var cityCodex = inputCity;
+
+    var Longi;
+    var Latti;
+
+    var cityname = $("<h>")
+    cityname.addClass("h3")
+    var temp = $("<div>")
+    var humidity = $("<div>")
+    var wind = $("<div>")
+    var icon = $("<img>")
+    icon.addClass("icon");
+    var datetobe = $("<div>")
+
+    $(".forecast").addClass("list-group")
+    $(".forecast").append(cityname)
+    $(".forecast").append(datetobe)
+    $(".forecast").append(icon)
+    $(".forecast").append(temp)
+    $(".forecast").append(wind)
+    $(".forecast").append(humidity)
+
+    var thegeourl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityCodex + "," + countryCode + "&limit=5&appid=9dd247337ad4b6b047a2f9a30dacaf93"
+
+    fetch(thegeourl)
+
+        //converts the response into JSON. This will return the JSON formatted response as ahead in the code
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+            Longi = data[0].lon;
+            Latti = data[0].lat;
+        })
+
+    //use Latti and Longi to fetch the weather
+    var weather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + Latti + "&lon=" + Longi + "&exclude=minutely,hourly,alerts&units=imperial&appid=9dd247337ad4b6b047a2f9a30dacaf93";
+
+    fetch(weather)
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //console.log(data)
+
+            weatherIcon = data.current.weather[0].icon;
+            imgSrc = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+            icon.attr('src', imgSrc)
+
+            cityname.text(cityCodex);
+            //translate utc to the date
+            var date = new Date(date.current.dt * 1000);
+            dateTime.text("(" + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear() + ")");
+
+            temp.text("Temperature: " + data.curent.temp + "°F");
+            humidity.text("Humidity: " + data.current.humidity + " %");
+            wind.text("Wind Speed: " + data.current.wind_speed + " MPH");
+
+            // WHEN I view future weather conditions for that city
+            // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+            //using the data from previous fetch and display the 5 day weather data
+            for (var i = 1; i < 6; i++) {
+
+                var Container = $("<div>")
+                this["futureDate" + i] = $("<h>")
+                this["futureIcon" + i] = $("<img>")
+                this["futureTemp" + i] = $("<div>")
+                this["futureWind" + i] = $("<div>")
+                this["futureHumidity" + i] = $("<div>")
+
+                //translate utc to the date
+                this["forecastDay" + i] = new Date(data.daily[i].dt * 1000);
+
+                (this["futureDate" + i]).text(((this["forecastDay" + i]).getMonth() + 1) + "/" + (this["forecastDay" + i]).getDate() + "/" + (this["forecastDay" + i]).getFullYear());
+                (this["futureTemp" + i]).text("Temperature: " + data.daily[i].temp.day + " F");
+                (this["futureWind" + i]).text("Wind: " + data.daily[i].wind_speed + " MPH");
+                (this["futureHumidity" + i]).text("Humidity: " + data.daily[i].humidity + " %");
+                (this["weatherIcon" + i]) = data.daily[i].weather[0].icon;
+
+                DateimgSrc = "https://openweathermap.org/img/wn/" + (this["weatherIcon" + i]) + ".png";
+                (this["futureIcon" + i]).attr('src', DateimgSrc)
+
+                $(".five-day").append(blueContainer)
+                blueContainer.append((this["futureDate" + i]));
+                blueContainer.append((this["futureIcon" + i]));
+                blueContainer.append((this["futureTemp" + i]));
+                blueContainer.append((this["futureWind" + i]));
+                blueContainer.append((this["futureHumidity" + i]));
+
+                blueContainer.addClass("weather-card")
+            }
+
+        })
+}
 
 
-let date = new Date();
+
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
+//get local storage info
+function getStorage() {
+    var currentList = localStorage.getItem("city");
+    if (currentList !== null) {
+        freshList = JSON.parse(currentList);
+        return freshList;
+    } else {
+        freshList = [];
+    }
+    return freshList;
+}
+//add info to local
+function addInfo (n) {
+    var addedList = getStorage();
+
+    if (historyList.includes(inputCity) === false){
+        addedList.push(n);
+    }
+
+    localStorage.setItem("city", JSON.stringify(addedList));
+};
+//render history
+function renderInfo() {
+    var historyList = getStorage();
+    for (var i = 0; i < historyList.length; i++) {
+        var inputCity = historyList[i];
+        var searchCity = $("<div>")
+        searchCity.attr('id', inputCity)
+        searchCity.text(inputCity)
+        searchCity.addClass("h4")
+
+        $(".history").append(searchCity)
+    }
+};
+
+renderInfo();
+/*let date = new Date();
 
 $("SearchTerm").keypress(function (event) {
 
@@ -75,7 +250,7 @@ function getCurrentConditions(response) {
     const city = $("<h4>").addClass("card-title").text(response.name);
     const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
     const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + Temp + "°F");
-    const humidity = $("<p>").addClass("card-text current-humdity").text("Humidity: " + response.main.humidity + "%");
+    const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
     const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed is: " + response.wind.speed + "MPH");
     const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
 
@@ -130,4 +305,4 @@ function getCurrentForecast() {
         }
     });
 
-}
+}*/
